6b2add074b065c636a17778e0804f4133b0a11cd
Prajwal Sitaula <163001705+CrypticLuminary@users.noreply.github.com>
2026-09-20 21:18:06 +0545

    Route responsive HTML rendering endpoint

--- backend/email_rendering/urls.py
+++ backend/email_rendering/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import MJMLRenderView
+from .views import HTMLRenderView, MJMLRenderView
 
 urlpatterns = [
     path("mjml/", MJMLRenderView.as_view(), name="email-render-mjml"),
+    path("html/", HTMLRenderView.as_view(), name="email-render-html"),
 ]
