667f1dd0b4d7c24cdbcd61a8a20a01e20e4e0a3f
Prajwal Sitaula <163001705+CrypticLuminary@users.noreply.github.com>
2026-09-20 13:50:03 +0545

    Route EmailDesign endpoint

--- backend/email_generation/urls.py
+++ backend/email_generation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ContentPlanView
+from .views import ContentPlanView, EmailDesignView
 
 urlpatterns = [
     path("plan/", ContentPlanView.as_view(), name="email-content-plan"),
+    path("design/", EmailDesignView.as_view(), name="email-design"),
 ]
