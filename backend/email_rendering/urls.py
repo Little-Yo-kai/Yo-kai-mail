4ddc0be0081863254bc76e17d4b9981cad914d6b
Prajwal Sitaula <163001705+CrypticLuminary@users.noreply.github.com>
2026-09-20 21:42:57 +0545

    Route browser screenshot endpoint

--- backend/email_rendering/urls.py
+++ backend/email_rendering/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import HTMLRenderView, MJMLRenderView
+from .views import EmailScreenshotView, HTMLRenderView, MJMLRenderView
 
 urlpatterns = [
     path("mjml/", MJMLRenderView.as_view(), name="email-render-mjml"),
     path("html/", HTMLRenderView.as_view(), name="email-render-html"),
+    path(
+        "screenshot/",
+        EmailScreenshotView.as_view(),
+        name="email-render-screenshot",
+    ),
 ]
